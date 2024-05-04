8f60a834f01aa10089aefb763e976a4e3765b3ab
Gabriel Tutor <gabdotcode01@gmail.com>
2024-05-03 19:54:14 -1200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.gryffindor',
+  appName: 'gryffindor',
+  webDir: 'dist'
+};
+
+export default config;
